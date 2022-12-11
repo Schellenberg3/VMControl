2cc9117c83b9e6602f5a0b6cd80147762e59ce08
Schellenberg3 <schellenberg.andrew@gmail.com>
2022-12-11 16:27:56 -0500

    add rlbench fork with custom edits

---
+++ vmcontrol/RLBench/examples/rlbench_gym.py
@@ -0,0 +1,19 @@
+import gym
+import rlbench.gym
+
+env = gym.make('reach_target-state-v0')
+# env = gym.make('slid_block_to_targe-v0')
+
+# env = gym.make('reach_target_easy-visiondepthmask-v0')
+
+training_steps = 120
+episode_length = 40
+for i in range(training_steps):
+    if i % episode_length == 0:
+        print('Reset Episode')
+        obs = env.reset()
+    obs, reward, terminate, _ = env.step(env.action_space.sample())
+    env.render()  # Note: rendering increases step time.
+
+print('Done')
+env.close()
